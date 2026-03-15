ca571592473de343a8a831ce9cbc7fb73e0df2be
Chaitreya <cbhelkar55@gmail.com>
2026-03-15 11:49:08 +0530

    Header | Login | Create user Added a dummy header file and included it in base.html Added a login and create user html page, added navigation between these two pages Will be adding functionality for login and create user in views in next commit

--- chadeo/urls.py
+++ chadeo/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+
+app_name = 'chadeo'
+
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("login/", views.login, name="login"),
+    path("signup", views.create_user , name="create_user")
 ]
--- chadeo/views.py
+++ chadeo/views.py
@@ -2,5 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
     return render(request, "chadeo/index.html")
+
+def create_user(request):
+    if request.method == "POST" :
+        username = request.POST["username"]
+        password = request.POST["password"]
+        email = request.POST["email"]
+
+    else :
+        return render(request, "chadeo/create_user.html")
+
+def login(request):
+    if request.method == "POST" :
+        username = request.POST["username"]
+        password = request.POST["password"]
+        email = request.POST["email"]
+
+    else :
+        return render(request, "chadeo/login.html")
