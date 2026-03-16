996b752d60167898eb0ec819674d30722b118d7f
Chaitreya <cbhelkar55@gmail.com>
2026-03-16 18:39:10 +0530

    Functionality for login_user, create_user, logout_user
    login_user, create_user and logout_user view are working now.
    Also login required is added to the index view and the header now shows the logged in user

--- chadeo/urls.py
+++ chadeo/urls.py
@@ -6,6 +6,7 @@ app_name = 'chadeo'
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login/", views.login, name="login"),
-    path("signup", views.create_user , name="create_user")
+    path("login/", views.login_user, name="login_user"),
+    path("signup/", views.create_user , name="create_user"),
+    path("logout/", views.logout_user, name="logout_user"),
 ]
--- chadeo/views.py
+++ chadeo/views.py
@@ -1,25 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 
-
+@login_required(login_url="/chadeo/login")
 def index(request):
-    return render(request, "chadeo/index.html")
+    user = request.user
+    
+    return render(request, "chadeo/index.html",{"user": user})
+
 
 def create_user(request):
     if request.method == "POST" :
         username = request.POST["username"]
         password = request.POST["password"]
         email = request.POST["email"]
-
+        try:
+            user = User.objects.get(username=username)
+            print("user already exists")
+            return redirect('chadeo:login_user') 
+        except User.DoesNotExist:
+            print("User not found")
+            user = User.objects.create_user(username=username,email=email,password=password)
+            user.save()
+            return redirect('chadeo:login_user') 
     else :
         return render(request, "chadeo/create_user.html")
 
-def login(request):
+def login_user(request):
     if request.method == "POST" :
         username = request.POST["username"]
         password = request.POST["password"]
-        email = request.POST["email"]
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('chadeo:index')
+        else:
+            return redirect('chadeo:login_user')
 
     else :
         return render(request, "chadeo/login.html")
+    
+
+def logout_user(request):
+    logout(request)
+    print("logout success")
+    return redirect('chadeo:login_user')
